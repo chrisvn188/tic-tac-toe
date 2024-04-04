9687f4448b69338a0052d3046ac7f107f3e02af4
chrisvn188 <chrisbui.dev188@gmail.com>
2024-04-04 19:35:26 -0400

    Add scoreboard

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const Gameboard = (function () {
-    let board = ['', '', '', '', '', '', '', '', ''];
+    let board = ['', '', '', '', '', '', '', '', '']
 
-    const getBoard = () => board.map((cell) => cell);
+    const getBoard = () => board.map((cell) => cell)
 
     const reset = () => {
-        board = ['', '', '', '', '', '', '', '', ''];
-    };
+        board = ['', '', '', '', '', '', '', '', '']
+    }
 
     const addMarker = (position, marker) => {
         if (board[position] === '') {
-            board[position] = marker;
+            board[position] = marker
         }
-    };
+    }
 
     const checkBoardFullOfMarkers = () => {
-        return board.every((cell) => cell !== '');
-    };
+        return board.every((cell) => cell !== '')
+    }
 
-    const checkEmptyCell = (position) => board[position] === '';
+    const checkEmptyCell = (position) => board[position] === ''
 
     return {
         getBoard,
@@ -25,13 +25,18 @@ const Gameboard = (function () {
         addMarker,
         checkEmptyCell,
         checkBoardFullOfMarkers,
-    };
-})();
+    }
+})()
 
 const Player = function (marker) {
-    const getMarker = () => marker;
-    return { getMarker };
-};
+    let score = 0
+    const getMarker = () => marker
+    const getScore = () => score
+    const updateScore = () => {
+        score = score + 1
+    }
+    return { getMarker, getScore, updateScore }
+}
 
 const GameController = (function () {
     const winConditions = [
@@ -43,116 +48,147 @@ const GameController = (function () {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-    ];
-    const playerX = Player('X');
-    const playerO = Player('O');
+    ]
+    const playerX = Player('X')
+    const playerO = Player('O')
 
-    let currentPlayer = playerX;
+    let scoreBoard = [0, 0]
 
-    let gameOver = false;
+    let currentPlayer = playerX
+
+    let gameOver = false
 
     const switchTurn = () =>
         (currentPlayer =
             currentPlayer.getMarker() === playerX.getMarker()
                 ? playerO
-                : playerX);
+                : playerX)
 
     const checkWin = (board) => {
         return winConditions.some((condition) =>
             condition.every(
                 (position) => board[position] === currentPlayer.getMarker()
             )
-        );
-    };
+        )
+    }
+
+    const getScoreBoard = () => {
+        return scoreBoard
+    }
+
+    const updateScoreBoard = () => {
+        scoreBoard.splice(0, scoreBoard.length)
+        scoreBoard.push(playerX.getScore(), playerO.getScore())
+    }
+
+    const resetScoreBoard = () => {
+        scoreBoard = [0, 0]
+    }
 
     const addMove = (position) => {
-        if (gameOver || !Gameboard.checkEmptyCell(position)) return;
+        if (gameOver || !Gameboard.checkEmptyCell(position)) return
 
-        Gameboard.addMarker(position, currentPlayer.getMarker());
+        Gameboard.addMarker(position, currentPlayer.getMarker())
 
         if (checkWin(Gameboard.getBoard())) {
-            gameOver = true;
+            gameOver = true
+            currentPlayer.updateScore()
+            updateScoreBoard()
             DisplayController.showResultModal(
                 `${currentPlayer.getMarker()} wins this round!`
-            );
-            return;
+            )
+            DisplayController.updateScoreText()
+            return
         }
 
         if (Gameboard.checkBoardFullOfMarkers()) {
-            gameOver = true;
-            DisplayController.showResultModal(`It's a tie!`);
-            return;
+            gameOver = true
+            DisplayController.showResultModal(`It's a tie!`)
+            return
         }
 
-        switchTurn();
-    };
+        switchTurn()
+    }
+
+    const resetBoard = () => {
+        Gameboard.reset()
+        currentPlayer = playerX
+        gameOver = false
+    }
 
     const resetGame = () => {
-        Gameboard.reset();
-        currentPlayer = playerX;
-        gameOver = false;
-    };
+        resetBoard()
+        scoreBoard = [0, 0]
+    }
 
-    return { addMove, resetGame };
-})();
+    return { addMove, resetGame, resetBoard, getScoreBoard, updateScoreBoard }
+})()
 
 const DisplayController = (function () {
-    const gameBoardElement = document.querySelector('#game-board');
-    const restartButtonElement = document.querySelector('#restart-button');
-    const resultModal = document.querySelector('#result-modal');
-    const restartButtonOnModal = document.querySelector('#restart-on-modal');
-    const resultText = document.querySelector('#result-text');
+    const gameBoardElement = document.querySelector('#game-board')
+    const restartButtonElement = document.querySelector('#restart-button')
+    const resultModal = document.querySelector('#result-modal')
+    const playNewGameBtn = document.querySelector('#play-new-game-btn')
+    const resultText = document.querySelector('#result-text')
+    const markerX = document.querySelector('.marker-x')
+    const markerO = document.querySelector('.marker-o')
 
     const createCell = (value, position) => {
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('cell');
-        cellElement.textContent = value;
-        cellElement.setAttribute('data-index', position);
-        return cellElement;
-    };
+        const cellElement = document.createElement('div')
+        cellElement.classList.add('cell')
+        cellElement.textContent = value
+        cellElement.setAttribute('data-index', position)
+        return cellElement
+    }
 
     const displayBoard = (board) => {
-        // remove all old children, rendering new children
         while (gameBoardElement.lastElementChild) {
-            gameBoardElement.removeChild(gameBoardElement.lastElementChild);
+            gameBoardElement.removeChild(gameBoardElement.lastElementChild)
         }
 
         board.forEach((value, index) => {
-            const newCell = createCell(value, index);
-            gameBoardElement.append(newCell);
-        });
-    };
+            const newCell = createCell(value, index)
+            gameBoardElement.append(newCell)
+        })
+    }
+
+    const updateScoreText = () => {
+        markerX.setAttribute('data-score', GameController.getScoreBoard()[0])
+        markerO.setAttribute('data-score', GameController.getScoreBoard()[1])
+    }
 
     const updateResultText = (text) => {
-        resultText.textContent = text;
-    };
+        resultText.textContent = text
+    }
 
     const showResultModal = (text) => {
-        resultModal.showModal();
-        updateResultText(text);
-    };
+        resultModal.showModal()
+        updateResultText(text)
+    }
 
     const render = () => {
-        displayBoard(Gameboard.getBoard());
-    };
+        displayBoard(Gameboard.getBoard())
+    }
 
     gameBoardElement.addEventListener('click', (e) => {
-        GameController.addMove(+e.target.dataset.index);
-        render();
-    });
+        GameController.addMove(+e.target.dataset.index)
+        render()
+    })
 
     restartButtonElement.addEventListener('click', () => {
-        GameController.resetGame();
-        render();
-    });
+        GameController.resetGame()
+        updateScoreText()
+        render()
+    })
 
-    restartButtonOnModal.addEventListener('click', () => {
-        resultModal.close();
-        GameController.resetGame();
-        render();
-    });
+    playNewGameBtn.addEventListener('click', () => {
+        resultModal.close()
+        GameController.resetBoard()
+        updateScoreText()
+        render()
+    })
 
-    return { render, showResultModal };
-})();
+    return { render, showResultModal, updateScoreText }
+})()
 
-DisplayController.render();
+DisplayController.render()
